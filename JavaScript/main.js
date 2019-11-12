0fd6669d3150c118aaf120ba672aa1bbd5fa94c5
MAKAYLA Reese <reese2596@students.cptc.edu>
2019-11-12 14:40:48 -0800

    Retrieving product is functional

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -8,12 +8,29 @@ window.onload = function () {
 function initializeBuyButtons() {
     var buyBtn = document.querySelectorAll("div.buy");
     for (var i = 0; i < buyBtn.length; i++) {
-        var currBtn = buyBtn[i];
-        currBtn.onclick = buyProduct;
+        var currBuyBtn = buyBtn[i];
+        currBuyBtn.onclick = buyProduct;
     }
 }
 function buyProduct() {
-    console.log("You clicked buy");
+    //console.log("You clicked buy");
+    var currBuyBtn = this;
+    /* console.log("The current div");
+    console.log(currBuyBtn); */
+    var currProdDiv = currBuyBtn.parentElement;
+    /* console.log("The parent Product div");
+    console.log(currProdDiv); */
+    ///Grab the product off the page 
+    ///add title, price, etc.
+    var prod = new Product();
+    prod.title = currProdDiv.querySelector("div.title").innerHTML;
+    //console.log(currProdDiv.querySelector("div.title"));
+    var price = currProdDiv.querySelector("div.price").innerHTML;
+    price = price.replace("$", "");
+    prod.price = parseFloat(price);
+    prod.discription = currProdDiv.querySelector("div.description").innerHTML;
+    /*alert("Do you want to add " + prod.title + ", " + prod.discription
+        + "to cart for " + prod.price);*/
 }
 /**
  * Represents a single shopping cart item
--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -10,11 +10,32 @@ window.onload = function(){
 function initializeBuyButtons() {
     let buyBtn = document.querySelectorAll("div.buy");
     for (let i = 0; i < buyBtn.length; i++) {
-        let currBtn = <HTMLElement>buyBtn[i];
-        currBtn.onclick = buyProduct;
+        let currBuyBtn = <HTMLElement>buyBtn[i];
+        currBuyBtn.onclick = buyProduct;
     }
 }
 
 function buyProduct() {
-    console.log("You clicked buy")
+    //console.log("You clicked buy");
+    let currBuyBtn = <HTMLElement> this;
+    /* console.log("The current div");
+    console.log(currBuyBtn); */
+
+    let currProdDiv = currBuyBtn.parentElement;
+    /* console.log("The parent Product div");
+    console.log(currProdDiv); */
+
+    ///Grab the product off the page 
+    ///add title, price, etc.
+    let prod = new Product();
+    prod.title = currProdDiv.querySelector("div.title").innerHTML;
+    //console.log(currProdDiv.querySelector("div.title"));
+    let price = currProdDiv.querySelector("div.price").innerHTML;
+    price = price.replace("$", "");
+    prod.price = parseFloat(price);
+    prod.discription = currProdDiv.querySelector("div.description").innerHTML;
+
+    /*alert("Do you want to add " + prod.title + ", " + prod.discription 
+        + "to cart for " + prod.price);*/
 }
+
